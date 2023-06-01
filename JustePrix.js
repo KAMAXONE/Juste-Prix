c1124b42844f647102f898a924bda82cab1c7be5
MathisDurdilly <119574532+KAMAXONE@users.noreply.github.com>
2023-06-01 09:37:24 +0200

    Update JustePrix.js

--- JustePrix.js
+++ JustePrix.js
@@ -1,60 +1,71 @@
-import * as random from 'random';
-import * as time from 'time';
-var nbErreur, nbHasard, rep, repTest, t1, t2, valeurMax, valeurMaxNb, valeurMin;
-console.log("*" * 100);
-console.log(" " * 30, "BIENVENUE SUR LE JEU DU JUSTE PRIX !");
-console.log("*" * 100);
-console.log("Pour jouer c'est tr\u00e8s simple , il vous suffit de donner la valeur maximal et ensuite de trouver le nombre qui se trouve entre 1 et votre valeur maximal .");
-console.log("A la fin s'affiche la quantit\u00e9 de nombre que vous avez tap\u00e9 ET le temp que vous avez mis . Bon courage et amusez vous bien !!!");
-valeurMin = 1;
+// Paramètres du jeu
+//génère un nombre aléatoire entre 1 et 100//
+var minNumber = 1;
+var maxNumber = 100;
+var targetNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 
-while (true) {
-  console.log();
-  valeurMax = input("Quelle sera la valeur maximal ? : ");
-  valeurMaxNb = valeurMax.isdigit();
 
-  if (valeurMaxNb === false) {
-    console.log("vous devez entrer un nombre !");
-  } else {
-    if (valeurMaxNb === true) {
-      break;
+// Timer
+//non opérationel pour le moment//
+var timer = document.getElementById("timer");
+var temps = 60;
+
+function startTimer() {
+    while (temps > 0) {
+        sleep(1000);
+        temps = temps - 1;
+        if (temps > 1) {
+            timer.innerHTML = temps + "secondes";
+        }
+        else if (temps < 1) {
+        }
+        else {
+            timer.innerHTML = temps + "seconde";
+        }
     }
-  }
+    timer.innerHTML = "finiiiii !";
 }
 
-valeurMax = Number.parseInt(valeurMax);
-nbHasard = random.randint(valeurMin, valeurMax);
-rep = 0;
-nbErreur = [];
-t1 = time.time();
-
-while (rep !== nbHasard) {
-  console.log("");
-  rep = input("quelle est votre choix ?: ");
-  repTest = rep.isdigit();
-
-  if (repTest === false) {
-    console.log("vous devez entrer un nombre !");
-  } else {
-    if (repTest === true) {
-      rep = Number.parseInt(rep);
-      nbErreur.append(rep);
-      console.log("");
-
-      if (rep < nbHasard) {
-        console.log(" " * 25, "+");
-        console.log();
-      }
-
-      if (rep > nbHasard) {
-        console.log(" " * 25, "-");
-        console.log();
-      }
-    }
-  }
+// Sleep
+function sleep(ms) {
+    return new Promise(
+      resolve => setTimeout(resolve, ms)
+    );
 }
 
-t2 = time.time() - t1;
-t2 = Number.parseInt(t2);
-console.log("bravo vous avez trouver la bonne r\u00e9ponse !");
-console.log("vous avez trouv\u00e9 la r\u00e9ponse au bout de {0} essais et au bout de {1} secondes.".format(nbErreur.length, t2));
+// Vérifier la proposition
+function checkGuess() {
+    var input = document.getElementById("guessInput");
+    var output = document.getElementById("guessOutput");
+    var guessList = document.getElementById("guessList");
+    var guess = parseInt(input.value);
+
+//si la valeur entrée n'est pas un nombre//
+    if (isNaN(guess)) {
+        alert("Veuillez entrer un nombre valide !");
+        return;
+    }
+
+//si la valeur entrée n'est pas comprise entre 1 et 100//
+    if (guess < minNumber || guess > maxNumber) {
+        alert("Le nombre doit être compris entre " + minNumber + " et " + maxNumber + " !");
+        return;
+    }
+
+//si la valeur entrée est comprise entre 1 et 100 lancer le timer//
+    if (guess > minNumber || guess < maxNumber) {
+        startTimer();
+    }
+
+if (guess < targetNumber) {
+        output.innerHTML = "Le nombre est plus grand que " + guess + " !";
+        guessList.innerHTML += "<li>" + guess + "< </li>";
+    } else if (guess > targetNumber) {
+        output.innerHTML = "Le nombre est plus petit que " + guess + " !";
+        guessList.innerHTML += "<li>" + guess + "> </li>";
+    }else {
+        output.innerHTML = "F\u00e9licitations, vous avez trouv\u00e9 le juste prix !";
+        guessList.innerHTML += "<li>" + guess + "= </li>";
+    }
+}
+
